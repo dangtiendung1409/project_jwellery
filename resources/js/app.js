75dbd9017c70ca6e002b3bcfb863935e5c587ac7
dungdang <dangdunghn1409@gmail.com>
2025-05-28 18:18:27 +0700

    setup vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './routes.js';
+
+import {
+    Alert,
+    Upload,
+    Modal,
+    Steps,
+} from 'ant-design-vue';
+
+import 'ant-design-vue/dist/reset.css';
+
+const app = createApp(App);
+const pinia = createPinia();
+
+// Sử dụng router và mount ứng dụng
+app.use(pinia);
+app.use(Alert);
+app.use(Steps);
+app.use(Upload);
+app.use(Modal);
+
+app.use(router).mount("#app");
